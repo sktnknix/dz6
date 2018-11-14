528f0c781d7540dc718f4efffec0b4ec880112d6
sktnknix <44968442+sktnknix@users.noreply.github.com>
2018-11-14 13:37:01 +0300

    Add files via upload

---
+++ dz6_1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import re
+input_reg = input('Enter expression, ex.: + 2 3: ')
+input_pattern = r'[+\-*/][ ]\d[ ]\d'
+match = re.fullmatch(input_pattern, input_reg)
+if not match:
+    print('enter expression with spaces like this: + 2 3')
+else:
+    input_list = []
+    for i in input_reg:
+        input_list.append(i)
+    input_list[0], input_list[2] = input_list[2], input_list[0]
+    input_list = ''.join(input_list)
+    try:
+        eval(input_list)
+    except ZeroDivisionError:
+        print('Error ! Divizion by zero')
+    else:
+        print(eval(input_list))
+    finally:
+        print('Exiting')
+
+
+
